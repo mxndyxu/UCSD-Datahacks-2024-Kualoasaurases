9101a0d8682fa1df75d47fe885cd3097968feea1
Mac.Ari <d.aritra@outlook.com>
2024-04-07 15:21:15 -0700

    Combined ML with Map

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,6 +4,25 @@ import pylab
 import folium
 from streamlit_folium import st_folium 
 import matplotlib
+import tensorflow as tf
+import numpy as np
+import keras
+
+
+# load model, set cache to prevent reloading
+@st.cache_resource(show_spinner=True)
+def load_model():
+    model = keras.models.load_model("./models/dinosaur_classifier.keras")
+    return model
+
+
+def load_image(image):
+    img = tf.image.decode_jpeg(image, channels=3)
+    img = tf.cast(img, tf.float32)
+    img /= 255.0
+    img = tf.image.resize(img, (224, 224))
+    img = tf.expand_dims(img, axis=0)
+    return img
 
 
 def return_color_generator(NUM_COLORS, cmap='terrain'):
@@ -22,7 +41,7 @@ def return_color_generator(NUM_COLORS, cmap='terrain'):
 # SOURCE: BugBytes from YouTube
 # caching data to avoid reloading data every time the page is refreshed with decorator
 @st.cache_data
-def read_data(data_loc='../data/filtered_dino_fossil_locations.csv'):
+def read_data(data_loc='./data/filtered_dino_fossil_locations.csv'):
     data = list()
 
     # data frame of dinosaur fossil locations across the world
@@ -53,17 +72,45 @@ def main():
     st.markdown('Welcome to our project for the 2024 UCSD DataHacks competition created by Aritra Das, Asif Mahdin, Luke Taylor, and Mandy Xu! Our project classifies an image of a dinosaur using transfer learning from a pre-trained network and finetuned to our dinosaurs dataset. An interactive map is displayed with the highlighted locations of where the classified dinosaur\'s fossils were found in the world.')
     st.markdown('To start, upload an image of a dinosaur. Once classified, explore the interactive map!')
 
+    with st.spinner("Loading Model...."):
+        model = load_model()
+
+    classes = [
+            "Ankylosaurus",
+            "Brachiosaurus",
+            "Compsognathus",
+            "Corythosaurus",
+            "Dilophosaurus",
+            "Dimorphodon",
+            "Gallimimus",
+            "Pachycephalosaurus",
+            "Parasaurolophus",
+            "Spinosaurus",
+            "Stegosaurus",
+            "Triceratops",
+            "Tyrannosaurus_Rex",
+            "Velociraptor",
+        ]
+    
     # File uploader for images
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         # Display uploaded image
         st.image(uploaded_file, use_column_width=True)
-    
+
+        st.write("Predicting Class...")
+
+        with st.spinner("Classifying..."):
+            img_tensor = load_image(uploaded_file.read())
+            pred = model.predict(img_tensor)
+            pred_class = classes[np.argmax(pred)]
+            st.write("Predicted Class:", pred_class)
+
     # Dataframe preview of fossil locations
     data = read_data()
-    st.subheader('Dataframe Preview')
-    st.dataframe(data.head())
+    # st.subheader('Dataframe Preview')
+    # st.dataframe(data.head())
 
     data_class_names = list(data['class_name'].unique())
     m = folium.Map(location=(40.841852219046864, 56.94639806488843), zoom_start=2) # map
